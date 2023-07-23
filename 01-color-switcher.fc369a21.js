8c3bf80006ac29c6a69b7fdd337cf88b3b0fa799
Gigapatut <125769111+Gigapatut@users.noreply.github.com>
2023-07-23 18:10:33 +0000

    Deploying to gh-pages from @ Gigapatut/goit-js-hw-09@e6845746b8484d58ed6df572b5e8606ebc24c623 🚀

---
+++ 01-color-switcher.fc369a21.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0;e.addEventListener("click",(()=>{d.disabled=!1,e.disabled=!0,timerId=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)})),d.addEventListener("click",(()=>{clearInterval(timerId),e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.fc369a21.js.map
